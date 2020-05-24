96898194e070258ff27cec4a4327264b8e3e4c5b
vineeth <b20vineeth>
2020-05-24 08:37:26 +0530

    introducing Text area and validation

--- src/common/util/TextField/RenderField.jsx
+++ src/common/util/TextField/RenderField.jsx
@@ -6,15 +6,20 @@ export const renderField = ({
     meta: { touched, error, warning }
   }) => (
     <div className="form-group">
-         <label htmlFor={label}>{label}</label>
-             <input {...input} placeholder={label} type={type} className="form-control" />
+
+           {label?<Label label={label}/>:""}
+            {type=='textarea' ? <textarea   {...input} class="form-control"  type={type} /> :
+             <input {...input} placeholder={label} type={type} className="form-control" />}
            {touched &&
              ((error && <span className="errorMsg"> {
                error
                }</span>) ||
              (warning && <span className="warningMsg">{warning}</span>))}
-      
+           
           </div>
   )
   
-  
+function Label(props) {
+
+  return  <label htmlFor={props.label}>{props.label}</label>;
+}
--- src/common/util/Validation/StringValidation.jsx
+++ src/common/util/Validation/StringValidation.jsx
@@ -2,7 +2,8 @@
 export const required = value => (value || typeof value === 'number' ? undefined : 'Required')
 export const maxLength = max => value =>
     value && value.length > max ? `Must be ${max} characters or less` : undefined
-export const maxLength15 = maxLength(15)
+export const maxLength15 = maxLength(15) 
+export const maxLength20 = maxLength(20) 
 export const minLength = min => value =>
     value && value.length < min ? `Must be ${min} characters or more` : undefined
 export const minLength3 = minLength(3)
